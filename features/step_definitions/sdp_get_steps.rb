38f94484a685f33e7e288710b307cc8e0ce8a1d4
Steve Loveless <steve.loveless@gmail.com>
2010-12-21 20:44:14 -0800

    Added pending steps

---
+++ features/step_definitions/sdp_get_steps.rb
@@ -0,0 +1,11 @@
+Given /^an SDP file that contains all possible spec'ed SDP fields$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I create an SDP object from the file$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^all fields and values are represented as Hashes$/ do
+  pending # express the regexp above with the code you wish you had
+end
