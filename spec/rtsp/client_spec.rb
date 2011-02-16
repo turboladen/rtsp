dd255aed6b248076fbc0a1c69b5502567fa2a093
Steve Loveless <steve.loveless@gmail.com>
2011-02-16 12:53:14 -0800

    Added tests for #server_url=

--- spec/rtsp/client_spec.rb
+++ spec/rtsp/client_spec.rb
@@ -6,4 +6,21 @@ describe RTSP::Client do
   it "increments the sequence number after receiving an OK response" do
 
   end
+
+  context "#server_url" do
+    before :each do
+      @client = RTSP::Client.new "rtsp://localhost"
+    end
+
+    it "allows for changing the server URL on the fly" do
+      @client.server_uri.to_s.should == "rtsp://localhost"
+      @client.server_url = "rtsp://localhost:8080"
+      @client.server_uri.to_s.should == "rtsp://localhost:8080"
+    end
+
+    it "raises when passing in something other than a String" do
+      @client.server_uri.to_s.should == "rtsp://localhost"
+      lambda { @client.server_url = [] }.should raise_error
+    end
+  end
 end
