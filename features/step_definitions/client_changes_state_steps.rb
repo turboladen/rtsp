eb68e29857b5b024af3df4a9f891fc639cf62a25
Steve Loveless <steve.loveless@gmail.com>
2011-03-09 13:01:12 -0800

    Added first cuke test for checking state changes

--- features/step_definitions/client_changes_state_steps.rb
+++ features/step_definitions/client_changes_state_steps.rb
@@ -1,14 +1,17 @@
 Given /^I haven't made any RTSP requests$/ do
   RTSP::Client.configure { |config| config.log = false }
-  url = "rtsp://64.202.98.91/sa.sdp"
-  @client = RTSP::Client.new url
-  @init_state = @client.session_state
 end
 
 When /^I issue an "([^"]*)" request$/ do |request_type|
+  mock_socket = double "MockSocket", :send => "", :recvfrom => [OPTIONS_RESPONSE]
+
+  url = "rtsp://fake-rtsp-server/some_path"
+  @client = RTSP::Client.new url, :socket => mock_socket
+  @initial_state = @client.session_state
+
   @client.send request_type.to_sym
 end
 
 Then /^the state stays the same$/ do
-  pending # express the regexp above with the code you wish you had
+  @client.session_state.should == @initial_state
 end
