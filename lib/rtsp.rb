efc74260fe5f3d341ff97654e99669f8e78c982a
Steve Loveless <steve.loveless@gmail.com>
2011-03-08 23:35:53 -0800

    Removed unused constants

--- lib/rtsp.rb
+++ lib/rtsp.rb
@@ -5,7 +5,5 @@ require File.expand_path(File.dirname(__FILE__) + '/rtsp/version')
 # This base module simply defines properties about the library.  See child
 # classes/modules for the meat.
 module RTSP
-  WWW = 'http://github.com/turboladen/rtsp'
-  LIBRARY_ROOT = File.dirname(__FILE__)
-  PROJECT_ROOT = Pathname.new(LIBRARY_ROOT).parent
+  
 end
