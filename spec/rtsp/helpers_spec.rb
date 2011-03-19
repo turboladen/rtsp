30e178c2f3ffd38534ff66ba223003c8f3c6eda1
Steve Loveless <steve.loveless@gmail.com>
2011-03-19 01:08:42 -0700

    Added tests for helpers

---
+++ spec/rtsp/helpers_spec.rb
@@ -0,0 +1,40 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require 'rtsp/helpers'
+
+class HelperTest
+  include RTSP::Helpers
+end
+
+describe RTSP::Helpers do
+  describe "#build_resource_uri_from" do
+    before do
+      @my_object = HelperTest.new
+    end
+
+    context "parses the resource URL to a URI" do
+      it "with scheme, IP, path; port defaults to 554" do
+        uri = @my_object.build_resource_uri_from "rtsp://64.202.98.91/sa.sdp"
+        uri.scheme.should == "rtsp"
+        uri.host.should == "64.202.98.91"
+        uri.port.should == 554
+        uri.path.should == "/sa.sdp"
+      end
+
+      it "with IP, path; port defaults to 554; scheme defaults to 'rtsp'" do
+        uri = @my_object.build_resource_uri_from "64.202.98.91/sa.sdp"
+        uri.scheme.should == "rtsp"
+        uri.host.should == "64.202.98.91"
+        uri.port.should == 554
+        uri.path.should == "/sa.sdp"
+      end
+
+      it "with scheme, IP, port" do
+        uri = @my_object.build_resource_uri_from "rtsp://64.202.98.91"
+        uri.scheme.should == "rtsp"
+        uri.host.should == "64.202.98.91"
+        uri.port.should == 554
+        uri.path.should == ""
+      end
+    end
+  end
+end
