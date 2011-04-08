d1f8a1144811a4a0cdbf3b511268c4f57b0fcd68
Steve Loveless <steve.loveless@gmail.com>
2011-04-07 17:52:35 -0700

    Added preliminary Transport header parser

---
+++ lib/rtsp/transport_parser.rb
@@ -0,0 +1,39 @@
+require 'parslet'
+
+module RTSP
+  class TransportParser < Parslet::Parser
+    rule(:transport_specifier) do
+      match('[A-Z]').repeat(3).as(:protocol) >> forward_slash >>
+          match('[A-Z]').repeat(3).as(:profile) >>
+          (forward_slash >> match('[A-Z]').repeat(3).as(:lower_transport)).maybe
+    end
+
+    rule(:broadcast_type) do
+      str('unicast')
+    end
+
+    rule(:client_port) do
+      str('client_port=') >> number.as(:rtp) >> dash >> number.as(:rtcp)
+    end
+
+    rule(:interleaved) do
+      str('interleaved=') >> number.as(:rtp_channel) >> dash >>
+          number.as(:rtcp_channel)
+    end
+
+    rule(:number)         { match('[\d]').repeat }
+    rule(:forward_slash)  { match('[/]') }
+    rule(:semi_colon)     { match('[;]') }
+    rule(:dash)           { match('[-]') }
+
+    rule(:header_field) do
+      transport_specifier >>
+          (semi_colon >> broadcast_type.as(:broadcast_type)).maybe >>
+          (semi_colon >> client_port.as(:client_port)).maybe >>
+          (semi_colon >> interleaved.as(:interleaved)).maybe
+
+    end
+
+    root :header_field
+  end
+end
---
+++ spec/rtsp/transport_parser_spec.rb
@@ -0,0 +1,54 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require 'rtsp/transport_parser'
+
+describe RTSP::TransportParser do
+  before do
+    @parser = RTSP::TransportParser.new
+  end
+
+  describe "a basic Transport header" do
+    before do
+      transport = "RTP/AVP;unicast;client_port=9000-9001"
+      @result = @parser.parse transport
+    end
+
+    it "extracts the protocol" do
+      @result[:protocol].should == 'RTP'
+    end
+
+    it "extracts the profile" do
+      @result[:profile].should == 'AVP'
+    end
+
+    it "extracts the broadcast type" do
+      @result[:broadcast_type].should == 'unicast'
+    end
+
+    it "extracts the client RTP port" do
+      @result[:client_port][:rtp].should == '9000'
+    end
+
+    it "extracts the client RTCP port" do
+      @result[:client_port][:rtcp].should == '9001'
+    end
+  end
+
+  describe "a TCP binary interleaved Transport header" do
+    before do
+      transport = "RTP/AVP/TCP;unicast;interleaved=0-1"
+      @result = @parser.parse transport
+    end
+
+    it "extracts the lower transport type" do
+      @result[:lower_transport].should == 'TCP'
+    end
+
+    it "extracts the interleaved RTP channel" do
+      @result[:interleaved][:rtp_channel].should == '0'
+    end
+
+    it "extracts the interleaved RTCP channel" do
+      @result[:interleaved][:rtcp_channel].should == '1'
+    end
+  end
+end
