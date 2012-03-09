727f14b0033ba188f2dd273dc90f40c4b23ff8ae
Steve Loveless <steve.loveless@gmail.com>
2012-03-08 17:26:46 -0800

    Transfer header parser fixes & improvements.  Closes gh-11.

--- lib/rtsp/transport_parser.rb
+++ lib/rtsp/transport_parser.rb
@@ -7,20 +7,21 @@ module RTSP
   # other requests.
   class TransportParser < Parslet::Parser
     rule(:transport_specifier) do
-      match('[A-Z]').repeat(3).as(:streaming_protocol) >> forward_slash >>
-          match('[A-Z]').repeat(3).as(:profile) >>
-          (forward_slash >> match('[A-Z]').repeat(3).as(:transport_protocol)).maybe
+      match('[A-Za-z]').repeat(3).as(:streaming_protocol) >> forward_slash >>
+        match('[A-Za-z]').repeat(3).as(:profile) >>
+        (forward_slash >> match('[A-Za-z]').repeat(3).as(:transport_protocol)).maybe
     end
 
     rule(:broadcast_type) do
-      str('unicast')
+      str('unicast') | str('multicast')
     end
 
     rule(:destination) do
-      str('destination=') >> ip_address
+      str('destination=') >> ip_address.as(:destination)
     end
+
     rule(:source) do
-      str('source=') >> ip_address
+      str('source=') >> ip_address.as(:source)
     end
 
     rule(:client_port) do
@@ -33,14 +34,40 @@ module RTSP
 
     rule(:interleaved) do
       str('interleaved=') >> number.as(:rtp_channel) >> dash >>
-          number.as(:rtcp_channel)
+        number.as(:rtcp_channel)
+    end
+
+    rule(:ttl) do
+      str('ttl=') >> match('[\d]').repeat(1,3).as(:ttl)
+    end
+
+    rule(:port) do
+      str('port=') >> match('[\d]').repeat(1,5).as(:rtp) >>
+      dash.maybe >> match('[\d]').repeat(1,5).as(:rtcp).maybe
+    end
+
+    rule(:ssrc) do
+      str('ssrc=') >> match('[0-9A-Fa-f]').repeat(8).as(:ssrc)
+    end
+
+    rule(:channel) do
+      str('channel=') >> match('[\w]').repeat(1,3).as(:channel)
+    end
+
+    rule(:address) do
+      str('address=') >> match('[\S]').repeat.as(:address)
+    end
+
+    rule(:mode) do
+      str('mode=') >> str('"').maybe >> match('[A-Za-z]').repeat.as(:mode) >>
+      str('"').maybe
     end
 
     rule(:ip_address) do
       match('[\d]').repeat(1,3) >> str('.') >>
-          match('[\d]').repeat(1,3) >> str('.') >>
-          match('[\d]').repeat(1,3) >> str('.') >>
-          match('[\d]').repeat(1,3)
+        match('[\d]').repeat(1,3) >> str('.') >>
+        match('[\d]').repeat(1,3) >> str('.') >>
+        match('[\d]').repeat(1,3)
     end
 
     rule(:number)         { match('[\d]').repeat }
@@ -50,13 +77,18 @@ module RTSP
 
     rule(:header_field) do
       transport_specifier >>
-          (semi_colon >> broadcast_type.as(:broadcast_type)).maybe >>
-          (semi_colon >> destination.as(:destination)).maybe >>
-          (semi_colon >> source.as(:source)).maybe >>
-          (semi_colon >> client_port.as(:client_port)).maybe >>
-          (semi_colon >> server_port.as(:server_port)).maybe >>
-          (semi_colon >> interleaved.as(:interleaved)).maybe
-
+        (semi_colon >> broadcast_type.as(:broadcast_type)).maybe >>
+        (semi_colon >> destination).maybe >>
+        (semi_colon >> source).maybe >>
+        (semi_colon >> client_port.as(:client_port)).maybe >>
+        (semi_colon >> server_port.as(:server_port)).maybe >>
+        (semi_colon >> interleaved.as(:interleaved)).maybe >>
+        (semi_colon >> ttl).maybe >>
+        (semi_colon >> port.as(:port)).maybe >>
+        (semi_colon >> ssrc).maybe >>
+        (semi_colon >> channel).maybe >>
+        (semi_colon >> address).maybe >>
+        (semi_colon >> mode).maybe
     end
 
     root :header_field
--- lib/rtsp/version.rb
+++ lib/rtsp/version.rb
@@ -1,4 +1,4 @@
 module RTSP
   # rtsp version
-  VERSION = "0.3.0"
+  VERSION = "0.3.1"
 end
