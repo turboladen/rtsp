f730eafcbe4385d03da4db694d166cc58d27f7e5
Steve Loveless <steve.loveless@gmail.com>
2012-11-11 20:42:08 -0800

    Fixes for Session and Transport values

--- lib/ext/hash_ext.rb
+++ lib/ext/hash_ext.rb
@@ -19,8 +19,51 @@ class Hash
       header_name = key.to_header_name
 
       if value.is_a?(Hash) || value.is_a?(Array)
-        values = if header_name == "Content-Type"
+        values = case header_name
+        when "Content-Type"
           values_to_s(value, ", ")
+        when "Session"
+          v = "#{value[:session_id]}"
+
+          if value.has_key?(:timeout)
+            v << value[:timeout]
+          end
+
+          v
+        when "Transport"
+          v = "#{value[:streaming_protocol]}/#{value[:profile]}"
+
+          if value.has_key?(:lower_transport)
+            v << "/#{value[:lower_transport]}"
+          end
+
+          v << ";#{value[:broadcast_type]}"
+
+          if value.has_key? :interleaved
+            v << ";interleaved=#{value[:interleaved][:start]}-#{value[:interleaved][:end]}"
+          end
+
+          if value.has_key? :append
+            v << ";append"
+          end
+
+          [:destination, :ttl, :layers, :ssrc].each do |k|
+            if value.has_key?(k)
+              v << ";#{k}=#{value[k]}"
+            end
+          end
+
+          if value.has_key? :mode
+            v << ";mode=\"#{value[:mode]}\""
+          end
+
+          [:client_port, :server_port, :port].each do |k|
+            if value.has_key?(k)
+              v << ";#{k}=#{value[k][:rtp]}-#{value[k][:rtcp]}"
+            end
+          end
+
+          v
         else
           values_to_s(value)
         end
@@ -72,11 +115,7 @@ class Hash
   def values_to_s(values, separator=";")
     result = values.inject("") do |values_string, (header_field, header_field_value)|
       if header_field.is_a? Symbol
-        values_string << if header_field == :session_id
-          header_field_value.to_s
-        else
-          "#{header_field}=#{header_field_value}"
-        end
+        "#{header_field}=#{header_field_value}"
       elsif header_field.is_a? Hash
         values_string << values_to_s(header_field)
       else
